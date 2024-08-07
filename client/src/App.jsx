b1fc9306616ea51ec6489fe34066c97770281371
gautamkumar1 <gautamraaz936@gmail.com>
2024-08-07 16:32:26 +0530

    Created Home, Login, Register Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import Navbar from "./sourceComponents/Navbar"
+import Footer from "./sourceComponents/Footer"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 
 function App() {
-  
+
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </>
   )
 }
